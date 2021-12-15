9d5ddbb34ac51d06d19b9b33f8aca39c7f6c48f2
Jinnie-J <jjesweet@naver.com>
2021-12-15 15:47:57 +0900

    상품 도메인 개발

---
+++ src/test/java/jpaproject/jpashop/service/ItemServiceTest.java
@@ -0,0 +1,35 @@
+package jpaproject.jpashop.service;
+
+import jpaproject.jpashop.domain.item.Item;
+import jpaproject.jpashop.repository.ItemRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
+public class ItemServiceTest {
+
+    @Autowired ItemService itemService;
+    @Autowired ItemRepository itemRepository;
+
+    @Test
+    public void 상품저장() throws Exception{
+        //given
+        Item item = new Item();
+        item.setName("itemA");
+
+        //when
+       itemService.saveItem(item);
+
+        //then
+        assertEquals(item, itemRepository.findOne(item.getId()));
+    }
+}
