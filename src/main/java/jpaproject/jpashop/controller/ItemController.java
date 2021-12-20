b182a06579192f6521de5feade62cf786a898203
Jinnie-J <jjesweet@naver.com>
2021-12-20 13:28:18 +0900

    상품 등록, 상품 목록 조회

---
+++ src/main/java/jpaproject/jpashop/controller/ItemController.java
@@ -0,0 +1,49 @@
+package jpaproject.jpashop.controller;
+
+import jpaproject.jpashop.domain.item.Book;
+import jpaproject.jpashop.domain.item.Item;
+import jpaproject.jpashop.service.ItemService;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class ItemController {
+
+    private final ItemService itemService;
+
+    @GetMapping("/items/new")
+    public String createForm(Model model){
+        model.addAttribute("form", new BookForm());
+        return "items/createItemForm";
+    }
+
+    @PostMapping("items/new")
+    public String create(BookForm form, BindingResult result){
+
+        Book book = new Book();
+        book.setName(form.getName());
+        book.setPrice(form.getPrice());
+        book.setStockQuantity(form.getStockQuantity());
+        book.setAuthor(form.getAuthor());
+        book.setIsbn(form.getIsbn());
+
+        itemService.saveItem(book);
+        return "redirect:/";
+    }
+
+    @GetMapping("/items")
+    public String list(Model model){
+        List<Item> items = itemService.findItems();
+        model.addAttribute("items",items);
+        return "items/itemList";
+    }
+}
